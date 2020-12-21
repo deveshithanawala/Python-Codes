98c51c589162ab056ab673d1362aa0c971a9466c
deveshithanawala <deveshi.m.thanawala@gmail.com>
2020-12-21 09:23:18 +0530

    Update2

---
+++ task4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# HackerRank Task 4:
+# 
+# PYTHON DIVISION
+# 
+# Take input a and b 
+# Add logic to print two lines. The first line should contain the result of integer division,  a//b . The second line should contain the result of float division, a / b .
+# 
+# No rounding or formatting is necessary.
+
+# In[1]:
+
+
+a=int(input())
+b=int(input())
+
+print(int(a//b))
+print(float(a/b))
+
+
+# In[ ]:
+
+
+
+
